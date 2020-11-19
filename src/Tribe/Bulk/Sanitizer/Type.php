2eafde08387ea52bfa31385e91201daf1415a7ac
Crisoforo Gaspar <hello@crisoforo.com>
2020-11-18 20:20:16 -0600

    EXT-235: Define sanitizers for type of imports.

--- src/Tribe/Bulk/Sanitizer/Type.php
+++ src/Tribe/Bulk/Sanitizer/Type.php
@@ -5,6 +5,7 @@ namespace Tribe\Extensions\EA_Additional_Options\Bulk\Sanitizer;
 class Type extends Sanitizer {
 	private $alias = [
 		'one time'  => 'manual',
+		'once'      => 'manual',
 		'scheduled' => 'schedule',
 	];
 
