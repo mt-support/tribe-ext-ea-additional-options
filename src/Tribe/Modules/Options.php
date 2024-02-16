fe90bf10e3a7aebf5c96dd61331f1c984633ebc8
András Guseo <andras@theeventscalendar.com>
2024-02-16 11:15:55 +0100

    Rework meridian logic

--- src/Tribe/Modules/Options.php
+++ src/Tribe/Modules/Options.php
@@ -119,20 +119,14 @@ class Options {
 			} else if ( $missing_event_details ) {
 				return $event;
 			} else {
-				// If there is a meridian and it's "pm" then adjust times.
-				if (
-					isset( $event['EventStartMeridian'] )
-					&& 'pm' === strtolower( $event['EventStartMeridian'] )
-					&& $event['EventStartHour'] < 12
-				) {
-					$event['EventStartHour'] += 12;
-				}
-				if (
-					isset( $event['EventEndMeridian'] )
-					&& 'pm' === strtolower( $event['EventEndMeridian'] )
-					&& $event['EventEndHour'] < 12
-				) {
-					$event['EventEndHour'] += 12;
+				// If there is a meridian, and it's "pm" then adjust times for 24h format.
+				if ( isset( $event['EventStartMeridian'] ) && isset( $event['EventEndMeridian'] ) ) {
+					if ( strtolower( $event['EventStartMeridian'] ) === 'pm' && $event['EventStartHour'] < 12 ) {
+						$event['EventStartHour'] += 12;
+					}
+					if ( strtolower( $event['EventEndMeridian'] ) === 'pm' && $event['EventEndHour'] < 12 ) {
+						$event['EventEndHour'] += 12;
+					}
 				}
 
 				$event['EventTimezone'] = str_replace( 'UTC', 'Etc/GMT', $event['EventTimezone'] );
