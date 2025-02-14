1825df04c5e67f7293a4d8468c730eef1971e1fb
Andras Guseo <andras@theeventscalendar.com>
2025-02-14 20:37:40 +0100

    remove old code

--- src/Tribe/Modules/Settings.php
+++ src/Tribe/Modules/Settings.php
@@ -37,11 +37,10 @@ class Settings {
 	}
 
 	public function hook() {
-		//add_action( 'admin_init', [ $this, 'add_settings' ] );
-		add_filter( 'tec_events_settings_tab_imports_fields', [ $this, 'new_add_settings' ] );
+		add_filter( 'tec_events_settings_tab_imports_fields', [ $this, 'add_additional_options' ] );
 	}
 
-	public function new_add_settings( $fields ) {
+	public function add_additional_options( $fields ) {
 		$content_block          = new Div( new Classes( [ 'tec-settings-form__content-section' ] ) );
 		$section_header_classes = new Classes( [ 'tec-settings-form__section-header', 'tec-settings-form__section-header--sub' ] );
 
@@ -141,83 +140,6 @@ class Settings {
 		$fields[] = $additional_options;
 
 		return $fields;
-	}
-
-	/**
-	 * Adds a new section of fields to Events > Settings > General tab, appearing after the "Map Settings" section
-	 * and before the "Miscellaneous Settings" section.
-	 *
-	 */
-	public function add_settings() {
-		$fields = [
-			self::PREFIX . 'heading'                         => [
-				'type' => 'html',
-				'html' => '<h3>' . esc_html__( 'Additional Options', 'tribe-ext-ea-additional-options' ) . '</h3>',
-			],
-			self::PREFIX . 'delete_duplicate_removed_events' => [
-				'type'            => 'radio',
-				'label'           => esc_html__( 'Delete Duplicate/Removed Events for Scheduled Imports', 'tribe-ext-ea-additional-options' ),
-				'tooltip'         => sprintf(
-				/* translators: %1$s: opening strong tag; %2$s: closing strong tag */
-					esc_html__(
-						'Check this box to delete events that are removed from the import source. This will also remove duplicates in the case where the source changes the unique identifier for an event. %1$sNOTE: If your "Event Update Authority" setting is "Do not re-import events...", this setting will have no effect.%2$s', 'tribe-ext-ea-additional-options'
-					),
-					'<strong>',
-					'</strong>'
-				),
-				'validation_type' => 'options',
-				'default'         => 'no',
-				'options'         => [
-					'no'        => __( 'Do not delete duplicate/removed events.', 'tribe-ext-ea-additional-options' ),
-					'trash'     => __( 'Send duplicate/removed events to trash.', 'tribe-ext-ea-additional-options' ),
-					'permanent' => __( 'Permanently delete duplicate/removed events.', 'tribe-ext-ea-additional-options' ),
-				],
-			],
-			self::PREFIX . 'link_directly_to_website_url'    => [
-				'type'            => 'radio',
-				'label'           => esc_html__( 'Link Directly to Website URL, Bypassing Default Event Page', 'tribe-ext-ea-additional-options' ),
-				'tooltip'         => sprintf(
-				/* translators: %1$s: opening strong tag; %2$s: closing strong tag */
-					esc_html__( 'Instead of linking to the Event page within The Events Calendar, enable this option so that visitors can be sent directly to the URL in the Website URL field. %1$sNOTE: This setting only affects legacy views and will not work in the upgraded views.%2$s', 'tribe-ext-ea-additional-options' ),
-					'<strong>',
-					'</strong>'
-				),
-				'validation_type' => 'options',
-				'default'         => 'no',
-				'options'         => [
-					'no'  => __( 'Link to the default single event page.', 'tribe-ext-ea-additional-options' ),
-					'yes' => __( 'Link directly to the event website URL', 'tribe-ext-ea-additional-options' ),
-				],
-			],
-			self::PREFIX . 'retain_line_breaks'              => [
-				'type'            => 'radio',
-				'label'           => esc_html__( 'Retain Line Breaks in Event Description', 'tribe-ext-ea-additional-options' ),
-				'tooltip'         => esc_html__( 'Some import sources allow for linebreaks. Choose whether to remove linebreaks or keep them.', 'tribe-ext-ea-additional-options' ),
-				'validation_type' => 'options',
-				'default'         => 'no',
-				'options'         => [
-					'no'  => __( 'Remove all line breaks from event descriptions.', 'tribe-ext-ea-additional-options' ),
-					'yes' => __( 'Retain all line breaks within event descriptions.', 'tribe-ext-ea-additional-options' ),
-				],
-			],
-			self::PREFIX . 'default_template'                => [
-				'type'            => 'dropdown',
-				'label'           => esc_html__( 'Block editor template', 'tribe-ext-ea-additional-options' ),
-				'tooltip'         => $this->get_template_tooltip(),
-				'default'         => false,
-				'validation_type' => 'options',
-				'options'         => $this->get_template_options(),
-				'can_be_empty'    => true,
-				'size'            => 'small',
-				'attributes'      => $this->get_template_attributes(),
-			],
-		];
-
-		$this->settings_helper->add_fields(
-			$fields, 'imports',
-			'tribe_aggregator_disable',
-			false
-		);
 	}
 
 	/**
